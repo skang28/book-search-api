4ba55eb89ef70fc2923e1a7c4fb32756a88d589a
Steven Kang <skang28@gmail.com>
2019-10-16 15:34:44 -0400

    fix API KEY

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const API_KEY = config.env.REACT_APP_BOOKSEARCH_API_KEY;
+const API_KEY = process.env.REACT_APP_BOOKSEARCH_API_KEY;
 
 class BookSearch extends React.Component {
     constructor(props) {
